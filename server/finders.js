15294d3b7560fc52b0a30d60fdf4210a947446ed
Alice Smith <alice@smith.com>
2024-01-30 23:20:47 -0500

    update

--- server/finders.js
+++ server/finders.js
@@ -4,7 +4,7 @@ exports.getCharacters = (req, res) => {
   characters
     .find({})
     .then((data) => {
-      res.status(200).callback(data);
+      res.status(200).send(data);
     })
     .catch((err) => {
       res.status(501).send({ alert: "Error getting all characters" }, err);
@@ -15,11 +15,11 @@ exports.getCharacter = (req, res) => {
   characters
     .findOne(req.params.id)
     .then((data) => {
-      res.status(200).callback(data);
+      res.status(200).send(data);
     })
     .catch((err) => {
       res
         .status(501)
-        .callback({ alert: `Error getting character with id ${id}` }, err);
+        .send({ alert: `Error getting character with id ${id}` }, err);
     });
 };
